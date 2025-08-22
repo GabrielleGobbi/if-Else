23d3fdcec8fbf98f27880cc1920288b108a24a86
Géssica Gabrielle Monteiro Gobbi <34252650+GabrielleGobbi@users.noreply.github.com>
2025-08-22 00:15:33 -0300

    Update 4-TaxExemption.test.ts

--- tests/4-TaxExemption.test.ts
+++ tests/4-TaxExemption.test.ts
@@ -1,19 +1,20 @@
 import { TaxExemption } from '../src/implementacao/4-TaxExemption';
 
-describe('TaxExemption', () => {
-  test('deve ser ISENTA quando renda mensal for menor ou igual a 3036', () => {
-    expect(TaxExemption(false, false, 3000)).toBe("ISENTA");
+describe('Exercício 04 - Isenção de Impostos', () => {
+  test('deve ser ISENTA quando aposentada', () => {
+    expect(TaxExemption(true, false, 5000000)).toBe("ISENTA");
   });
 
-  test('deve ser ISENTA quando rendimentos anuais forem menores ou iguais a 33888', () => {
-    expect(TaxExemption(false, false, 33000)).toBe("ISENTA");
+  test('deve ser ISENTA quando portadora de doença grave', () => {
+    expect(TaxExemption(false, true, 4000000)).toBe("ISENTA");
   });
 
-  test('deve ser ISENTA quando aposentada e com doença grave', () => {
-    expect(TaxExemption(true, true, 50000)).toBe("ISENTA");
+  test('deve ser VAZA LEAO quando rendimentos forem menores ou iguais a 28.559,70', () => {
+    expect(TaxExemption(false, false, 2000000))
+      .toBe("VAZA LEAO! JA TA DIFICIL SEM VOCE");
   });
 
-  test('deve ser PEGA LEAO quando não se enquadra nas isenções', () => {
-    expect(TaxExemption(false, false, 34000)).toBe("PEGA LEAO");
+  test('deve ser PEGA LEAO quando rendimentos forem maiores que 28.559,70 e sem isenções', () => {
+    expect(TaxExemption(false, false, 3000000)).toBe("PEGA LEAO");
   });
 });
